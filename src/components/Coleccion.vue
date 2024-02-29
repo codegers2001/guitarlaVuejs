765750517ec1589fc9d61792df43950ca77f0699
Lincoln Eulogio Huanca <137632394+codegers2001@users.noreply.github.com>
2024-02-29 00:09:18 -0500

    creamos la coleccion

---
+++ src/components/Coleccion.vue
@@ -0,0 +1,255 @@
+<template>
+  <section>
+    <h2 class="text-amber-600 font-bold text-center my-10 text-5xl">Nuestra Colección</h2>
+  </section>
+  <div class="container mx-auto grid grid-col md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_01.jpg" alt="guitarra 1"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_02.jpg" alt="guitarra 2"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_03.jpg" alt="guitarra 3"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_04.jpg" alt="guitarra 4"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_05.jpg" alt="guitarra 5"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_06.jpg" alt="guitarra 6"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_07.jpg" alt="guitarra 7"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_08.jpg" alt="guitarra 8"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_09.jpg" alt="guitarra 9"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_10.jpg" alt="guitarra 10"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_11.jpg" alt="guitarra 11"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img src="/public/img/guitarra_12.jpg" alt="guitarra 12"  />
+      </div>
+      <div>
+        <h3 class="uppercase text-2xl font-bold mb-3">Lukather</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error officiis
+          perspiciatis totam laboriosam illum omhic aspernatur?
+        </p>
+
+        <p class="text-amber-400 font-bold text-3xl my-6">$299</p>
+
+        <a
+          href="#"
+          class="bg-amber-500 text-xl hover:bg-amber-600 hover:cursor-pointer transition text-white py-2 px-10 inline-block font-bold mb-10"
+          >Agregar al Carrito</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2,
+p,
+a {
+  font-family: "Onest Variable", sans-serif;
+}
+</style>
