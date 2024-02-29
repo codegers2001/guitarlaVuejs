849b6a0acba0a8c8dd85fe3d9faf1fdadfdd3532
Lincoln Eulogio Huanca <137632394+codegers2001@users.noreply.github.com>
2024-02-29 00:09:34 -0500

    y luego creamos el footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Coleccion from "./components/Coleccion.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header />
+  <Coleccion />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script>
+// Supports weights 100-900
+import "@fontsource-variable/onest";
+</script>
+<template>
+    <footer class="mt-10">
+        <div class="bg-zinc-950 text-white text-center py-10">
+            <p class="text-lg">&copy; 2024 - GuitarLA - Todos los derechos reservados</p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+h1,
+p,
+a {
+  font-family: "Onest Variable", sans-serif;
+}
+</style>
